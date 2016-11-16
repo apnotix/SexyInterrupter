5ef459f4aa3f559c0e1b7c326c537d0617e2f8af
apnotix <apnotix2007@hotmail.de>
2016-11-16 22:07:32 +0100

    Spec versuch

--- core.lua
+++ core.lua
@@ -9,7 +9,6 @@ function SI:UpdateUI()
 	for i = 1, SI.numInterrupters do
 		if (not _G["SexyInterrupterRow" .. i]) then	
 			local interrupter = SI.interrupters[i];
-
 			local f = CreateFrame("Frame", "SexyInterrupterRow" .. i, SexyInterrupterAnchor)
 			
 			f:SetSize(20, 20)
@@ -56,20 +55,19 @@ end
 function SI:UpdateInterrupterStatus()
 	for i = 1, SI.numInterrupters do
 		local interrupter = SI.interrupters[i];
-		local row = _G["SexyInterrupterRow" .. i];
+		local row = _G["SexyInterrupterStatusBar" .. i];
+		local rowParent = _G["SexyInterrupterRow" .. i];
 
-		SendChatMessage('SexyInterrupter UpdateUI start ' .. i .. ' - ' .. row, 'party')
-		
-		if (row) then	
+		if row then	
 			if interrupter.offline then
-				row:Hide();
+				rowParent:Hide();
 			elseif interrupter.dead then
 				row.text:SetTextColor(1, 0, 0, 1);
 			elseif interrupter.afk then
 				row.text:SetTextColor(1, 1, 0, 1);
 			end
 			
-			row.text:SetText(interrupter.name);
+			row.text:SetText(interrupter.name .. ' - ' .. interrupter.role);
 		end
 	end
 end
@@ -96,6 +94,16 @@ function SI:UpdateInterrupters()
 		interrupter.ready = true;
 		interrupter.name = name;
 		interrupter.realm = realm;
+		
+		interrupter.role = UnitGroupRolesAssigned(Unit);
+		
+		if interrupter.role == 'HEALER' then
+			interrupter.prio = 3;
+		elseif interrupter.role == 'DAMAGER' then
+			interrupter.prio = 2;
+		elseif interrupter.role == 'TANK' then
+			interrupter.prio = 1;
+		end
 		
 		local class, classFileName = UnitClass(unit)
 		local color = RAID_CLASS_COLORS[classFileName]
@@ -129,11 +137,16 @@ function SI:UpdateInterrupters()
 	end
 end
 
+function SI_UNIT_FLAGS() 
+	SI_GROUP_ROSTER_UPDATE();
+end
+
 function SI_GROUP_ROSTER_UPDATE()
 	DEFAULT_CHAT_FRAME:AddMessage('SexyInterrupter: SI_GROUP_ROSTER_UPDATE');
 	
 	if IsInGroup() then
-		--SI:UpdateInterrupterStatus();
+		SI:UpdateInterrupters();
+		SI:UpdateInterrupterStatus();
 	end
 end
 
@@ -177,5 +190,6 @@ function SI:OnLoad()
 	DEFAULT_CHAT_FRAME:AddMessage('SexyInterrupter ' .. SI:GetVersion() .. ' loaded', 1, 0.5, 0);
 	
 	SIframe:UnregisterEvent("ADDON_LOADED");
+	SIframe:RegisterEvent("UNIT_FLAGS");
 	SIframe:RegisterEvent("GROUP_ROSTER_UPDATE");
 end
