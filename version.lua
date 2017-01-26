64ac30dbf9e862f82cf56da2e8dfa1702f522ed3
apnotix <apnotix2007@hotmail.de>
2017-01-26 19:24:45 +0100

    versioninfo

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 SexyInterrupter.versions = {
     {
-        id = "1.5",
-        title = 'SexyInterrupter v1.5',
+        id = "1.6",
+        title = 'SexyInterrupter v1.6',
         message = "<br />- Added a new notification system to display version information.<br />- Added a addon button on the minimap. Leftclick: toggle Frame, Rightclick: open settings"
     }
 }
