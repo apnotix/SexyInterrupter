8e4d4a7f1dc17e2d157ff0ce893e2da3f7a8c324
apnotix <apnotix2007@hotmail.de>
2017-02-03 17:01:38 +0100

    fixed translation key

--- version.lua
+++ version.lua
@@ -1,5 +1,13 @@
 SexyInterrupter.versions = {
     {
+        id = "1.63",
+        title = "SexyInterrupter v1.63",
+        message = "<br />- Fixed translations"
+    },{
+        id = "1.62",
+        title = "SexyInterrupter v1.62",
+        message = "<br />- Fixed interrupt output channel selector"
+    },{
         id = "1.61",
         title = 'SexyInterrupter v1.61',
         message = "<br />- Update lib files"
